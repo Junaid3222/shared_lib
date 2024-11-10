dd2fec887da985b9f94e899606707f06aaea0dad
Mohammed Junaid <junaid.ansari3222@gmail.com>
2024-11-10 21:43:37 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,3 +1,9 @@
-def call() {           
-  sh "mvn clean package"
+def call() {
+    def isWindows = System.getProperty('os.name').toLowerCase().contains('win')
+
+    if (isWindows) {
+        bat "mvn clean package" // Use 'bat' for Windows
+    } else {
+        sh "mvn clean package" // Use 'sh' for Linux/Unix
+    }
 }
