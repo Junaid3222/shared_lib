2795b0a917fdd937ed005ccdc57af5a6873d9ce2
Mohammed Junaid <junaid.ansari3222@gmail.com>
2024-11-10 21:39:23 +0530

    Update gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,7 +1,3 @@
-def call(repo) {
-   stage('clone') {
-      steps {
-         git url: 'https://github.com/Junaid3222/mvn-web-app.git'
-      }
-   }
+def call(repo) {           
+   git url: 'https://github.com/Junaid3222/mvn-web-app.git'
 }
